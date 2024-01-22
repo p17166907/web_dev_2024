8fe1d93a260c37cfd274399d29ba67e7ce58057e
GooseyEvan <56296788+p17166907@users.noreply.github.com>
2024-01-22 21:36:09 +0000

    hero desktop done

--- react-growth-app/src/App.js
+++ react-growth-app/src/App.js
@@ -1,10 +1,12 @@
 import './styles.css';
 import { Navbar } from './components/Navbar-component';
+import { Hero } from './components/Hero-component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ react-growth-app/src/components/Hero-component.js
@@ -0,0 +1,18 @@
+
+export function Hero() {
+
+    return (
+        <section className="hero" >
+            <div className="container">
+                <div className="hero-content">
+                    <h1 className="hero-heading text-xxl">A powerful solution to grow your startup. Fast!</h1>
+                    <p className="hero-content">Organize, collaborate, and track progress seamlessly with our one-stop-shop
+                        startup growth tool </p>
+                    <div className="hero-buttons">
+                        <a href="/getstarted" className="btn btn-primary ">Get Started</a>
+                        <a href="/bookdemo" className="btn"> <i class="fas fa-laptop" /> Book A Demo</a>
+                    </div>
+                </div>
+            </div>
+        </section>)
+}
