32176afa5e3ed2cde33f7f884ac2fd3dc801e9ee
GooseyEvan <56296788+p17166907@users.noreply.github.com>
2024-01-24 22:19:39 +0000

    built testimonial section

--- react-growth-app/src/App.js
+++ react-growth-app/src/App.js
@@ -2,12 +2,15 @@ import './styles.css';
 import { Navbar } from './components/Navbar-component';
 import { Hero } from './components/Hero-component';
 import { Video } from './components/Video-component';
+import { Testimonial } from './components/Testimonial-component';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Video />
+      <Testimonial />
     </div>
   );
 }
---
+++ react-growth-app/src/components/Testimonial-component.js
@@ -0,0 +1,27 @@
+export function Testimonial() {
+    return (
+        <section className="testimonials bg-dark">
+            <div className="container">
+                <h3 className="testimonials-heading text-xl">	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus quidem iusto est
+                    perferendis quaerat</h3>
+                <div className="testimonials-grid">
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, laudantium.</p>
+                        <p>Katherine</p>
+                        <p>Consectetur</p>
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, laudantium.</p>
+                        <p>Katherine</p>
+                        <p>Consectetur</p>
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, laudantium.</p>
+                        <p>Katherine</p>
+                        <p>Consectetur</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
