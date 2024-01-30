a5072a6b83b94c301e60f699dff054e26886d32a
GooseyEvan <56296788+p17166907@users.noreply.github.com>
2024-01-30 13:31:24 +0000

    attempt to make app user friendly

--- grwth-app-react-project/src/components/Navbar-component.js
+++ grwth-app-react-project/src/components/Navbar-component.js
@@ -1,18 +1,25 @@
+import React, { useState } from 'react';
 import logo from '../images/logo.png'; // Adjust the path as necessary
 
 export function Navbar() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="container">
                 <div className="logo">
                     <a href="/home"> <img src={logo} alt="logo" /> </a>
                 </div>
-                <div className='main-menu'>
+                <button className="hamburger" onClick={() => setIsNavExpanded(!isNavExpanded)}  >
+                    {/* Icon for hamburger menu (three horizontal lines) */}
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className={`main-menu ${isNavExpanded ? 'active' : ''}`}>
                     <ul>
                         <li><a href="/home">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/blog">Blog</a></li>
-                        <li><a href="/login"><i className="fas fa-user" /> Log in</a></li>
+                        <li><a href="/login"><i className="fas fa-user"></i> Log in</a></li>
                     </ul>
                 </div>
             </div>
